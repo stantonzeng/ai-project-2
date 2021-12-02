370d99a9ecacbd2b4a3971ccd778d354336fcf5a
Stantonzeng <Szeng010@ucr.edu>
2021-12-01 23:10:29 -0800

    Slight Fix

--- Main.py
+++ Main.py
@@ -1,37 +1,48 @@
 import numpy as np
 
-things = []
+def open_small(num):
+    things = []
 
-with open('Ver_2_CS170_Fall_2021_Small_data__86.txt', 'rt', encoding='utf-8') as f:
-    # lines = f.readlines()
-    for x in f:
-        things.append(x)
+    with open('Ver_2_CS170_Fall_2021_Small_data__{}.txt'.format(num), 'rt', encoding='utf-8') as f:
+        for x in f:
+            things.append(x)
+    return things
 
-feature = []
-
-features_list = []
-for i in range(0, 11):
-    features_list.append(feature)
-
-print(len(things))
-for i in things:
-    help = 1
-    count = 0
-    j = 3
-    for x in range(3, len(i)):
-        if help == -1 and i[x] == " ":
-            continue
-
-        if help == -1:
-            j = x
+def parse_text(things):
+    features_list = []
+    for i in range(0, 11):
+        features_list.append([])
+    
+    for i in things:
         help = 1
-        if x == len(i)-1:
-            features_list[count].append(i[j:x])
-        elif i[x] == " ": 
-            help = -1
-            features_list[count].append(i[j:x])
-            count = count + 1
+        count = 0
+        j = 3
+        for x in range(3, len(i)):
+            if help == -1 and i[x] == " ":
+                continue
 
-for i in features_list:
-    print(i)
+            if help == -1:
+                j = x
+            help = 1
+            
+            if x == len(i)-1:
+                nest_list = features_list[count]
+                nest_list.append(i[j:x])
+            elif i[x] == " ": 
+                help = -1
+                nest_list = features_list[count]
+                nest_list.append(i[j:x])
+                count = count + 1
 
+    return features_list
+
+
+def print_everything(features_list):
+    for i in features_list:
+        print(i)
+
+if __name__ == "__main__":
+    things = open_small(86)
+    features_list = parse_text(things)
+    # print_everything(features_list)
+    
